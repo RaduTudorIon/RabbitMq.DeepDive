c695ba174c008507378ba45b089270e83759264d
Ionut Radu <radutudorion@gmail.com>
2026-05-07 18:12:19 +0300

    Rabbitmq healthcheck fix

--- RabbitMq.DeepDive.AppHost/AppHost.cs
+++ RabbitMq.DeepDive.AppHost/AppHost.cs
@@ -4,7 +4,10 @@ var configPath = Path.Combine(builder.AppHostDirectory, "rabbitmq-config");
 var enabledPluginsPath = Path.Combine(configPath, "enabled_plugins");
 Console.WriteLine($"RabbitMQ config path: {configPath}");
 
-var rabbit = builder.AddRabbitMQ("rabbitmq")
+var rabbitUser = builder.AddParameter("rabbitmq-health-user", "guest");
+var rabbitPassword = builder.AddParameter("rabbitmq-health-password", "guest", secret: true);
+
+var rabbit = builder.AddRabbitMQ("rabbitmq", userName: rabbitUser, password: rabbitPassword)
     .WithManagementPlugin()
     .WithBindMount(configPath, "/etc/rabbitmq/conf.d")
     .WithBindMount(enabledPluginsPath, "/etc/rabbitmq/enabled_plugins");
@@ -14,12 +17,6 @@ var apiService = builder.AddProject<Projects.RabbitMq_DeepDive_ApiService>("apis
     .WithReference(rabbit)
     .WithEnvironment("ConnectionStrings__rabbitmq-management", rabbit.GetEndpoint("management"))
     .WaitFor(rabbit);
-
-builder.AddProject<Projects.RabbitMq_DeepDive_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
 
 builder.AddProject<Projects.RabbitMq_DeepDive_Producer>("producer")
     .WithHttpHealthCheck("/health")
