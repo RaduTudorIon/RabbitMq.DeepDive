05e23e0e05ec618e811103bfea9e2da48fa54218
Ionut Radu <radutudorion@gmail.com>
2026-06-08 09:15:06 +0300

    Added prometheus for metrics and grafana for dashboard

--- RabbitMq.DeepDive.AppHost/AppHost.cs
+++ RabbitMq.DeepDive.AppHost/AppHost.cs
@@ -2,15 +2,41 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var configPath = Path.Combine(builder.AppHostDirectory, "rabbitmq-config");
 var enabledPluginsPath = Path.Combine(configPath, "enabled_plugins");
+var prometheusConfigPath = Path.Combine(builder.AppHostDirectory, "prometheus");
+var grafanaProvisioningPath = Path.Combine(builder.AppHostDirectory, "grafana", "provisioning");
+var grafanaDashboardsPath = Path.Combine(builder.AppHostDirectory, "grafana", "dashboards");
 Console.WriteLine($"RabbitMQ config path: {configPath}");
 
 var rabbitUser = builder.AddParameter("rabbitmq-health-user", "guest");
 var rabbitPassword = builder.AddParameter("rabbitmq-health-password", "guest", secret: true);
 
-var rabbit = builder.AddRabbitMQ("rabbitmq", userName: rabbitUser, password: rabbitPassword)
-    .WithManagementPlugin()
+var rabbit = builder.AddRabbitMQ("rabbitmq", userName: rabbitUser, password: rabbitPassword, port: 5672)
+    .WithManagementPlugin(port: 15672)
     .WithBindMount(configPath, "/etc/rabbitmq/conf.d")
     .WithBindMount(enabledPluginsPath, "/etc/rabbitmq/enabled_plugins");
+
+var rabbitMqExporter = builder.AddContainer("rabbitmq-exporter", "kbudde/rabbitmq-exporter", "latest")
+    .WithHttpEndpoint(targetPort: 9419, name: "http")
+    .WithEnvironment("RABBIT_URL", "http://rabbitmq:15672")
+    .WithEnvironment("RABBIT_USER", "guest")
+    .WithEnvironment("RABBIT_PASSWORD", rabbitPassword)
+    .WithEnvironment("PUBLISH_PORT", "9419")
+    .WaitFor(rabbit);
+
+var prometheus = builder.AddContainer("prometheus", "prom/prometheus", "v2.55.1")
+    .WithHttpEndpoint(targetPort: 9090, name: "http")
+    .WithBindMount(prometheusConfigPath, "/etc/prometheus")
+    .WaitFor(rabbitMqExporter);
+
+var grafana = builder.AddContainer("grafana", "grafana/grafana-oss", "11.2.0")
+    .WithHttpEndpoint(targetPort: 3000, name: "http")
+    .WithEnvironment("GF_SECURITY_ADMIN_USER", "admin")
+    .WithEnvironment("GF_SECURITY_ADMIN_PASSWORD", "admin")
+    .WithEnvironment("GF_AUTH_ANONYMOUS_ENABLED", "true")
+    .WithEnvironment("GF_AUTH_ANONYMOUS_ORG_ROLE", "Viewer")
+    .WithBindMount(grafanaProvisioningPath, "/etc/grafana/provisioning")
+    .WithBindMount(grafanaDashboardsPath, "/etc/grafana/provisioning/dashboards/json")
+    .WaitFor(prometheus);
 
 var apiService = builder.AddProject<Projects.RabbitMq_DeepDive_ApiService>("apiservice")
     .WithHttpHealthCheck("/health")
--- RabbitMq.DeepDive.Consumer/Program.cs
+++ RabbitMq.DeepDive.Consumer/Program.cs
@@ -39,9 +39,44 @@ builder.Host.UseWolverine(opts =>
         rabbit.Password = "changeme";
         rabbit.VirtualHost = "TestVhost";
     })
-    .AutoProvision();
+    .AutoProvision()
+    .DisableDeadLetterQueueing()
+    .DeclareExchange("TestDirect.Exch", e =>
+    {
+        e.ExchangeType = Wolverine.RabbitMQ.ExchangeType.Direct;
+        e.IsDurable = true;
+    })
+    .DeclareExchange("TestFanout.Exch", e =>
+    {
+        e.ExchangeType = Wolverine.RabbitMQ.ExchangeType.Fanout;
+        e.IsDurable = true;
+    })
+    .DeclareExchange("TestTopic.Exch", e =>
+    {
+        e.ExchangeType = Wolverine.RabbitMQ.ExchangeType.Topic;
+        e.IsDurable = true;
+    })
+    .DeclareExchange("orders.dlx", e =>
+    {
+        e.ExchangeType = Wolverine.RabbitMQ.ExchangeType.Direct;
+        e.IsDurable = true;
+    })
+    .DeclareQueue("TestDirect.Q")
+    .DeclareQueue("TestFanout.Q")
+    .DeclareQueue("TestTopic.Q")
+    .DeclareQueue("orders")
+    .DeclareQueue("orders.dlq")
+    .BindExchange("TestDirect.Exch").ToQueue("TestDirect.Q", "test")
+    .BindExchange("TestFanout.Exch").ToQueue("TestFanout.Q", "")
+    .BindExchange("TestTopic.Exch").ToQueue("TestTopic.Q", "test.#")
+    .BindExchange("orders.dlx").ToQueue("orders.dlq", "orders.dead");
 
-    opts.ListenToRabbitQueue("orders");
+    opts.ListenToRabbitQueue("orders")
+        .DeadLetterQueueing(new DeadLetterQueue("orders.dlq", DeadLetterQueueMode.Native)
+        {
+            ExchangeName = "orders.dlx",
+            BindingName = "orders.dead"
+        });
 });
 
 var app = builder.Build();
